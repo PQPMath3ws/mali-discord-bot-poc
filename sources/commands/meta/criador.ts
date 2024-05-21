3be49b9a5bba11dc80542d14077cfd96760f071a
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2024-05-21 14:12:34 -0300

    Atualização do texto do comando /criador

--- sources/commands/meta/criador.ts
+++ sources/commands/meta/criador.ts
@@ -5,6 +5,6 @@ import { Command } from "../../interfaces/command";
 export const criador:Command = {
     data: new SlashCommandBuilder().setName("criador").setDescription("Quem criou esse bot?"),
     run: async (interaction) => {
-        await interaction.reply("**Esse bot foi desenvolvido pelo desenvolvedor *Mathews Martins*. Veja mais projetos dele em https://github.com/PQPMath3ws .**");
+        await interaction.reply("**Esse bot foi desenvolvido pelo desenvolvedor *Mathews Martins*. Saiba mais sobre ele em https://mathews.com.br .**");
     }
 };
